f487645898ee18894f4146da2310aa36539387d4
unknown <pro.designer2250@gmail.com>
2023-01-18 12:02:19 +0300

    Fixing replays

--- replays.py
+++ replays.py
@@ -15,19 +15,20 @@ otl = sheet.worksheet('OrdersTaskList')
 
 def process_message(msg, created_by):
     new_msg = str(msg)
-    ##### If it's a Quotation #####
-    if new_msg[2] == '1':
-        if otl.find(msg, in_column=5) is None:
-            otlTime = now.strftime("%d/%m/%Y %I:%M %p")
-            otlDefault = {'Created by': '', 'Create DateTime': otlTime, 'Customer Name': '', 'Customer': '', 'Quotation': '', 'Need Approval': '', 'Brand Managers': '', 'Financial': '', 'Approved': '', 'Creditlimit': '', 'Branch Manager': '', 'CL Financial': '', 'CL Approved': '', 'Finished Date': ''}
-            otlUpdate = {'Created by': created_by, 'Quotation': msg}
-            otlUpdate = dict(otlDefault, **otlUpdate)
-            otlData = list(otlUpdate.values())
-            otl.append_row(otlData, value_input_option='USER_ENTERED')
-            return pc.copy("تم اضافة الطلبية وجاري العمل على المطلوب")
-        else:
-            return pc.copy("الطلبية مكررة")
     try:
+        ##### If it's a Quotation #####
+        if new_msg[2] == '1':
+            if otl.find(msg, in_column=5) is None:
+                otlTime = now.strftime("%d/%m/%Y %I:%M %p")
+                otlDefault = {'Created by': '', 'Create DateTime': otlTime, 'Customer Name': '', 'Customer': '', 'Quotation': '', 'Need Approval': '', 'Brand Managers': '', 'Financial': '', 'Approved': '', 'Creditlimit': '', 'Branch Manager': '', 'CL Financial': '', 'CL Approved': '', 'Finished Date': ''}
+                otlUpdate = {'Created by': created_by, 'Quotation': msg}
+                otlUpdate = dict(otlDefault, **otlUpdate)
+                otlData = list(otlUpdate.values())
+                otl.append_row(otlData, value_input_option='USER_ENTERED')
+                return pc.copy("تم اضافة الطلبية وجاري العمل على المطلوب")
+            else:
+                return pc.copy("الطلبية مكررة")
+    except:
         textlang = str(detect_langs(msg))
         ##### Arabic Response #####
         if textlang.find('ar') == 1:
@@ -49,12 +50,9 @@ def process_message(msg, created_by):
                 return pc.copy("Hey!")
             elif new_msg in ("hello"):
                 return pc.copy("hi2")
-            else:
-                return pc.copy("I don't understand")
-        else: 
-            raise Exception
-    except Exception as e:
-        return pc.copy("Error: Invalid message")
+        else:
+            return pc.copy("I don't understand")
+        
         
 
     
