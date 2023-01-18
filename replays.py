7c967e2ecc689d143c4e30a2b1104c3ff369d2bf
unknown <pro.designer2250@gmail.com>
2023-01-18 11:42:33 +0300

    Modified Replays

--- replays.py
+++ replays.py
@@ -15,6 +15,18 @@ otl = sheet.worksheet('OrdersTaskList')
 
 def process_message(msg, created_by):
     new_msg = str(msg)
+    ##### If it's a Quotation #####
+    if msg[2] == '1':
+        if otl.find(msg, in_column=5) is None:
+            otlTime = now.strftime("%d/%m/%Y %I:%M %p")
+            otlDefault = {'Created by': '', 'Create DateTime': otlTime, 'Customer Name': '', 'Customer': '', 'Quotation': '', 'Need Approval': '', 'Brand Managers': '', 'Financial': '', 'Approved': '', 'Creditlimit': '', 'Branch Manager': '', 'CL Financial': '', 'CL Approved': '', 'Finished Date': ''}
+            otlUpdate = {'Created by': created_by, 'Quotation': msg}
+            otlUpdate = dict(otlDefault, **otlUpdate)
+            otlData = list(otlUpdate.values())
+            otl.append_row(otlData, value_input_option='USER_ENTERED')
+            return pc.copy("تم اضافة الطلبية وجاري العمل على المطلوب")
+        else:
+            return pc.copy("الطلبية مكررة")
     try:
         textlang = str(detect_langs(msg))
         ##### Arabic Response #####
@@ -29,28 +41,15 @@ def process_message(msg, created_by):
                 return pc.copy("هلا ومرحبا")
             elif new_msg in arabic_reshaper.reshape("السلام عليكم")[::-1]:
                 return pc.copy("وعليكم السلام ورحمة الله وبركاته")
-        else:
-            ##### English Response #####
-            new_msg = msg.lower()
-            if new_msg in ("hi"):
-                return pc.copy("Hey!")
-            elif new_msg in ("hello"):
-                return pc.copy("hi2")
-            else:
-                return pc.copy("I don't understand")
     except:
-        ##### If it's a Quotation #####
-        if msg[2] == '1':
-            if otl.find(msg, in_column=5) is None:
-                otlTime = now.strftime("%d/%m/%Y %I:%M %p")
-                otlDefault = {'Created by': '', 'Create DateTime': otlTime, 'Customer Name': '', 'Customer': '', 'Quotation': '', 'Need Approval': '', 'Brand Managers': '', 'Financial': '', 'Approved': '', 'Creditlimit': '', 'Branch Manager': '', 'CL Financial': '', 'CL Approved': '', 'Finished Date': ''}
-                otlUpdate = {'Created by': created_by, 'Quotation': msg}
-                otlUpdate = dict(otlDefault, **otlUpdate)
-                otlData = list(otlUpdate.values())
-                otl.append_row(otlData, value_input_option='USER_ENTERED')
-                return pc.copy("تم اضافة الطلبية وجاري العمل على المطلوب")
-            else:
-                return pc.copy("الطلبية مكررة")
+        ##### English Response #####
+        new_msg = msg.lower()
+        if new_msg in ("hi"):
+            return pc.copy("Hey!")
+        elif new_msg in ("hello"):
+            return pc.copy("hi2")
+        else:
+            return pc.copy("I don't understand")
         
 
     
